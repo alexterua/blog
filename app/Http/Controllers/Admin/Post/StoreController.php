cad0d1ac5cd0902de452b1e9d31ed2e7b855e916
alexterua <alexter.od.ua@gmail.com>
2022-03-22 21:44:07 +0200

    Destroy method dd() from StoreController

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -10,8 +10,8 @@ class StoreController extends BaseController
 {
     public function __invoke(StoreRequest $request)
     {
-        dd($request);
         $data = $request->validated();
+
         $this->service->store($data);
 
         return redirect()->route('admin.post.index');
--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -132,3 +132,4 @@
 <!-- /.content -->
 
 @endsection
+
