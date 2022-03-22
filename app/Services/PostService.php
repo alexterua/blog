4d3c68c069dd67c985895fa0334f97c646f31a47
alexterua <alexter.od.ua@gmail.com>
2022-03-22 22:24:33 +0200

    Added сруchecks for images and arrays in PostService

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -12,14 +12,23 @@ class PostService
     public function store(array $data)
     {
         try {
-            $tagIds = $data['tag_ids'];
-            unset($data['tag_ids']);
+            if (isset($data['tag_ids'])) {
+                $tagIds = $data['tag_ids'];
+                unset($data['tag_ids']);
+            }
 
-            $data['image_preview'] = Storage::disk('public')->put('images/post_preview', $data['image_preview']);
+            if (isset($data['image_preview'])) {
+                $data['image_preview'] = Storage::disk('public')->put('images/post_preview', $data['image_preview']);
+            }
+            if (isset($data['image_main'])) {
             $data['image_main'] = Storage::disk('public')->put('images/post_main', $data['image_main']);
+            }
 
             $post = Post::firstOrCreate($data);
-            $post->tags()->attach($tagIds);
+
+            if (isset($tagIds)) {
+                $post->tags()->attach($tagIds);
+            }
 
             return true;
         } catch (\Exception $exception) {
@@ -30,14 +39,24 @@ class PostService
     public function update(array $data, Post $post)
     {
         try {
-            $tagIds = $data['tag_ids'];
-            unset($data['tag_ids']);
+            if (isset($data['tag_ids'])) {
+                $tagIds = $data['tag_ids'];
+                unset($data['tag_ids']);
+            }
 
-            $data['image_preview'] = Storage::disk('public')->put('images/post_preview', $data['image_preview']);
-            $data['image_main'] = Storage::disk('public')->put('images/post_main', $data['image_main']);
+            if (isset($data['image_preview'])) {
+                $data['image_preview'] = Storage::disk('public')->put('images/post_preview', $data['image_preview']);
+            }
+            if (isset($data['image_main'])) {
+                $data['image_main'] = Storage::disk('public')->put('images/post_main', $data['image_main']);
+            }
 
             $post->update($data);
-            $post->tags()->sync($tagIds);
+
+            if (isset($tagIds)) {
+                $post->tags()->sync($tagIds);
+            }
+
 
             return $post;
         } catch (\Exception $exception) {
