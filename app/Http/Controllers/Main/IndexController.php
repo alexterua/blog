cd24831e81af9d98f2634dd8a58a31e67278d0ba
alexterua <alexter.od.ua@gmail.com>
2022-03-27 11:16:00 +0300

    Added output of posts for main page, with all posts, random posts carousel and popular posts list; added pagination

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Post;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return view('main.index');
+        $posts = Post::paginate(6);
+        $randomPosts = Post::get()->random(4);
+
+        $likedPosts = Post::withCount('likedUsers')->orderByDesc('liked_users_count')->get()->take(4);
+        $lastUpdatedPosts = Post::orderByDesc('updated_at')->get()->random(3);
+
+        return view('main.index', compact('posts', 'randomPosts', 'likedPosts', 'lastUpdatedPosts'));
     }
 }
