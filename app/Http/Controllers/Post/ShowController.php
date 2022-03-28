1bc11fb10d850776b7861161f584d3a1145a2b78
alexterua <alexter.od.ua@gmail.com>
2022-03-28 11:40:15 +0300

    Added functionality for Related Posts

--- app/Http/Controllers/Post/ShowController.php
+++ app/Http/Controllers/Post/ShowController.php
@@ -10,9 +10,13 @@ class ShowController extends Controller
 {
     public function __invoke(Post $post)
     {
-        $date = new Carbon($post->updated_at);
-        $date = $date->format("M d, Y • h:i a");
+        $dateUpdated = Carbon::parse($post->updated_at);
 
-        return view('post.show', compact('post', 'date'));
+        $relatedPosts = Post::where('category_id', $post->category->id)
+            ->where('id', '!=', $post->id)
+            ->get()
+            ->take(3);
+
+        return view('post.show', compact('post', 'dateUpdated', 'relatedPosts'));
     }
 }
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,7 +5,7 @@
     <main class="blog-post">
         <div class="container">
             <h1 class="edica-page-title" data-aos="fade-up">{{ $post->title }}</h1>
-            <p class="edica-blog-post-meta" data-aos="fade-up" data-aos-delay="200">{{ $date }} • {{ $post->category->title }} • {{ $post->comments->count() }} Comments</p>
+            <p class="edica-blog-post-meta" data-aos="fade-up" data-aos-delay="200">{{ $dateUpdated->format('M d, Y • h:i a') }} • {{ $post->category->title }} • {{ $post->comments->count() }} Comments</p>
             <section class="blog-post-featured-img" data-aos="fade-up" data-aos-delay="300">
                 <img src="{{ asset('storage/' . $post->image_main) }}" alt="{{ $post->title }}" class="w-100">
             </section>
@@ -20,27 +20,20 @@
                 <div class="col-lg-9 mx-auto">
                     <section class="related-posts">
                         <h2 class="section-title mb-4" data-aos="fade-up">Related Posts</h2>
-                        <div class="row">
-                            <div class="col-md-4" data-aos="fade-right" data-aos-delay="100">
-                                <img src="{{ asset('assets/images/blog_post_related_1.png') }}" alt="related post" class="post-thumbnail">
-                                <p class="post-category">Blog post</p>
-                                <h5 class="post-title">Front becomes an official Instagram</h5>
+                            <div class="row">
+                            @foreach($relatedPosts as $relatedPost)
+                                <div class="col-md-4" data-aos="fade-right" data-aos-delay="100">
+                                    <img src="{{ asset('storage/' . $relatedPost->image_preview) }}" alt="{{ $post->title }}" class="post-thumbnail" style="max-height: 200px">
+                                    <p class="post-category">{{ $relatedPost->category->title }}</p>
+                                    <a href="{{ route('post.show', $relatedPost->id) }}"><h5 class="post-title">{{ $relatedPost->title }}</h5></a>
+                                </div>
+                            @endforeach
                             </div>
-                            <div class="col-md-4" data-aos="fade-up" data-aos-delay="100">
-                                <img src="{{ asset('assets/images/blog_post_related_2.png') }}" alt="related post" class="post-thumbnail">
-                                <p class="post-category">Blog post</p>
-                                <h5 class="post-title">Front becomes an official Instagram</h5>
-                            </div>
-                            <div class="col-md-4" data-aos="fade-left" data-aos-delay="100">
-                                <img src="{{ asset('assets/images/blog_post_related_3.png') }}" alt="related post" class="post-thumbnail">
-                                <p class="post-category">Blog post</p>
-                                <h5 class="post-title">Front becomes an official Instagram</h5>
-                            </div>
-                        </div>
                     </section>
                     <section class="comment-section">
-                        <h2 class="section-title mb-5" data-aos="fade-up">Leave a Reply</h2>
+                        <h2 class="section-title mb-5" data-aos="fade-up">Leave a comment</h2>
                         <form action="/" method="post">
+                            @csrf
                             <div class="row">
                                 <div class="form-group col-12" data-aos="fade-up">
                                     <label for="comment" class="sr-only">Comment</label>
@@ -63,7 +56,7 @@
                             </div>
                             <div class="row">
                                 <div class="col-12" data-aos="fade-up">
-                                    <input type="submit" value="Send Message" class="btn btn-warning">
+                                    <input type="submit" value="Send Comment" class="btn btn-warning">
                                 </div>
                             </div>
                         </form>
